cce763e4ec028fae6063a8d7dd6bd78e1b854869
hgrafa <hugorafell@gmail.com>
2023-03-24 09:03:45 -0300

    adding sample about interpolation vs. concatenation

---
+++ exemplos/concatena.py
@@ -0,0 +1,11 @@
+name = 'SpiderMan'
+age = 23
+hasPower = True
+realName = 'Peter Parker'
+power = 'spider'
+
+identidadeContenada = 'Name = ' + name + ', age = ' + str(age)
+identidadeInterpolada = f'Name: {name}, age: {age}, hasPower:{hasPower}'
+
+print(identidadeContenada)
+print(identidadeInterpolada)
