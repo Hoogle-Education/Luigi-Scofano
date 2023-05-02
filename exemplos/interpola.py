73a888f0da2ce4c25004866f937b927b32186943
hgrafa <hugorafell@gmail.com>
2023-05-02 17:01:02 -0300

    adding recaps

---
+++ exemplos/interpola.py
@@ -0,0 +1,12 @@
+name = 'SpiderMan'
+age = 23
+hasPower = True
+realName = 'Peter Parker'
+power = 'spider'
+
+identidadeInterpolada = f'Name: {name}, age: {age}, hasPower: {hasPower}'
+
+identidadeConcatenada = 'Name ' + name + \
+    ', age: ' + age + ', hasPower: ' + hasPower
+
+print(identidadeInterpolada)
